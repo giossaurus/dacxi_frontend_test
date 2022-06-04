876b383cfa68d47979a3346b6767997dd9fd184a
giossaurus <91167613+giossaurus@users.noreply.github.com>
2022-06-03 21:17:56 -0300

    changing js ajax for axios

--- main.js
+++ main.js
@@ -1,23 +1,63 @@
-var btc = document.getElementById("bitcoin");
-var dacxi = document.getElementById("dacxi");
-var eth = document.getElementById("ethereum");
-var atom = document.getElementById("cosmos");
-var lunc = document.getElementById("luna-wormhole");
+let btc = document.getElementById("bitcoin");
+let dacxi = document.getElementById("dacxi");
+let eth = document.getElementById("ethereum");
+let atom = document.getElementById("cosmos");
+let lunc = document.getElementById("luna-wormhole");
+
+let currBtcPrice = 0;
+let currDacxiPrice = 0;
+let currEthPrice = 0;
+let currAtomPrice = 0;
+let currLuncPrice = 0; 
+
+let prevBtcPrice = 0;
+let prevDacxiPrice = 0;
+let prevEthPrice = 0;
+let prevAtomPrice = 0;
+let prevLuncPrice = 0; 
 
 
-var liveprice = {
-    "async" : true,
-    "scrossDomain" : true,
-    "url" : "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin.dacxi.ethereum.cosmos.luna-wormhole&vs_currencies=usd",
+axios.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,luna-wormhole,cosmos,dacxi&vs_currencies=usd").then(response => {
 
-    "method" : "GET";
-    "headers" : {}
-}
+    currBtcPrice = response.data.bitcoin.usd,
+    currDacxiPrice = response.data.dacxi.usd,
+    currEthPrice = response.data.ethereum.usd,
+    currAtomPrice = response.data.cosmos.usd,
+    currLuncPrice = response.data["luna-wormhole"].usd;
 
-$.ajax(liveprice).done(function (response) {
-    btc.innerHTML = response.bitcoin.usd;
-    dacxi.innerHTML = response.dacxi.usd;
-    eth.innerHTML = response.ethereum.usd;
-    atom.innerHTML = response.cosmos.usd;
-    lunc.innerHTML = response.luna.usd;
-});
+    if(currBtcPrice !== prevBtcPrice) btc.innerHTML = "$" + currBtcPrice; 
+    if(currDacxiPrice !== prevDacxiPrice) dacxi.innerHTML = "$" + currDacxiPrice;
+    if(currEthPrice !== prevEthPrice) eth.innerHTML = "$" + currEthPrice;
+    if(currAtomPrice !== prevAtomPrice) atom.innerHTML = "$" + currAtomPrice;
+    if(currLuncPrice !== prevLuncPrice) lunc.innerHTML = "$" + currLuncPrice;
+
+    prevBtcPrice = response.data.bitcoin.usd,
+    prevDacxiPrice = response.data.dacxi.usd,
+    prevEthPrice = response.data.ethereum.usd,
+    prevAtomPrice = response.data.cosmos.usd,
+    prevLuncPrice = response.data["luna-wormhole"].usd;
+    
+})
+
+setInterval(function() {
+    axios.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,luna-wormhole,cosmos,dacxi&vs_currencies=usd").then(response => {
+
+    currBtcPrice = response.data.bitcoin.usd,
+    currDacxiPrice = response.data.dacxi.usd,
+    currEthPrice = response.data.ethereum.usd,
+    currAtomPrice = response.data.cosmos.usd,
+    currLuncPrice = response.data["luna-wormhole"].usd;
+
+    if(currBtcPrice !== prevBtcPrice) btc.innerHTML = "$" + currBtcPrice; 
+    if(currDacxiPrice !== prevDacxiPrice) dacxi.innerHTML = "$" + currDacxiPrice;
+    if(currEthPrice !== prevEthPrice) eth.innerHTML = "$" + currEthPrice;
+    if(currAtomPrice !== prevAtomPrice) atom.innerHTML = "$" + currAtomPrice;
+    if(currLuncPrice !== prevLuncPrice) lunc.innerHTML = "$" + currLuncPrice;
+
+    prevBtcPrice = response.data.bitcoin.usd,
+    prevDacxiPrice = response.data.dacxi.usd,
+    prevEthPrice = response.data.ethereum.usd,
+    prevAtomPrice = response.data.cosmos.usd,
+    prevLuncPrice = response.data["luna-wormhole"].usd;
+    
+})},5000)
