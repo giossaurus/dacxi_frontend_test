6a079ae123fc107f1e89450623e317c29ac1dad8
giossaurus <91167613+giossaurus@users.noreply.github.com>
2022-06-04 22:51:08 -0300

    css styling

---
+++ theme.js
@@ -0,0 +1,30 @@
+// const inputContainer = document.querySelector('input')
+// const rootElement = document.documentElement
+
+// const lightTheme = {
+//     '--background-color': '#FFF',
+//     '--text-color': '#1a1a1a',
+//     'buttton-background-color': '#9b8afb',
+// }
+
+// const darkTheme = {
+//     '--background-color': '#232222;',
+//     '--text-color': '#FFF',
+//     'buttton-background-color': '#5925dc',
+// }
+
+// inputContainer.addEventListerner('change', function(){
+//     const isChecked = inputContainer.checked
+//     isChecked ? changeTheme(lightTheme) : changeTheme(lightTheme)
+// })
+
+// function changeTheme(theme) {
+//     console.log(theme)
+//     for (let prop in theme) {
+//         console.log(prop.theme[prop])
+//     }
+// }
+
+// function changeProperty(property.value) {
+//     rootElement.style.setProperty(property, value)
+// }
